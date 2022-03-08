cb28b0bbba9120f618c76259a52da8b1a34dabbe
ShadowCode007 <86188285+PRDaylyanov20@users.noreply.github.com>
2022-03-08 19:29:24 +0200

    Finnish navbar
    Navbar is now responsive

--- code-la-vista/JS/TopNav.js
+++ code-la-vista/JS/TopNav.js
@@ -1,3 +1,8 @@
 function Extend() {
     var x = document.getElementById("mt");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
