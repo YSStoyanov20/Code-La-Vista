361c584a63ad8a4f7a8ef6fee6bd3791f0038334
ShadowCode007 <86188285+PRDaylyanov20@users.noreply.github.com>
2022-03-13 19:19:04 +0200

    redo map.html
    Change navbar, color and remove js to be added in future
    
    Co-Authored-By: Kaloyan Dimov <86070624+KHDimov20@users.noreply.github.com>

--- code-la-vista/JS/TopNav.js
+++
@@ -1,8 +0,0 @@
-function Extend() {
-    var x = document.getElementById("mt");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
