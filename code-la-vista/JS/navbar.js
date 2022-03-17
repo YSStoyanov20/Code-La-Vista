8f7301d389d001095fda0d95c8fc8cc585493c50
ShadowCode007 <86188285+PRDaylyanov20@users.noreply.github.com>
2022-03-17 20:29:28 +0200

    Navbar fix
    Change the look of the navbar and home page. Add more logos and make css fix
    
    Co-Authored-By: Daniil Furnika <85349388+DVFurnika20@users.noreply.github.com>
    Co-Authored-By: Kaloyan Dimov <86070624+KHDimov20@users.noreply.github.com>

---
+++ code-la-vista/JS/navbar.js
@@ -0,0 +1,11 @@
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-100%";
+    }
+    prevScrollpos = currentScrollPos;
+}
