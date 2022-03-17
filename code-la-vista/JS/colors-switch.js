cead66da968ff01c96d4e49c3e04020f664aa430
ShadowCode007 <86188285+PRDaylyanov20@users.noreply.github.com>
2022-03-17 21:35:59 +0200

    Make custom scrollbar
    Co-Authored-By: Daniil Furnika <85349388+DVFurnika20@users.noreply.github.com>
    Co-Authored-By: Kaloyan Dimov <86070624+KHDimov20@users.noreply.github.com>

--- code-la-vista/JS/colors-switch.js
+++ code-la-vista/JS/colors-switch.js
@@ -2,7 +2,7 @@ function changeBg() {
     var background = document.getElementById('color');
     var scrollValue = window.scrollY;
 
-    if (scrollValue < 800) {
+    if (scrollValue < 1200) {
         background.classList.remove('colorChange')
     } else {
         background.classList.add('colorChange');
