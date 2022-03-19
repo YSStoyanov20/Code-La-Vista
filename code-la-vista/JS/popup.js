607e0c2ca2a61a55dd08beef0e2ac2733ba37c48
ShadowCode007 <86188285+PRDaylyanov20@users.noreply.github.com>
2022-03-19 13:04:10 +0200

    Finish about us page
    Finish about us page and make some changes to map and main page.
    
    Co-Authored-By: Daniil Furnika <85349388+DVFurnika20@users.noreply.github.com>
    Co-Authored-By: Kaloyan Dimov <86070624+KHDimov20@users.noreply.github.com>
    Co-Authored-By: Yordan Stoyanov <86049799+YSStoyanov20@users.noreply.github.com>

--- code-la-vista/JS/popup.js
+++ code-la-vista/JS/popup.js
@@ -1,3 +1,7 @@
 function togglePopup() {
     document.getElementById("popup-1").classList.toggle("active");
 }
+
+function togglePopup2() {
+    document.getElementById("popup-2").classList.toggle("active");
+}
