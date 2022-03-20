d68c302094a10a6b6af7a0c106656e50be4af5cd
ShadowCode007 <86188285+PRDaylyanov20@users.noreply.github.com>
2022-03-20 09:23:11 +0200

    Change popups
    Remove html code for popups and make it in JavaScript and fix map size problem

---
+++ code-la-vista/JS/data.js
@@ -0,0 +1,20 @@
+var data = {
+    "SlivenLandmarks": {
+        "title": "LoremL",
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe voluptatum sunt minus voluptatem quaerat quod earum tenetur officia quis.",
+        "link1": { "href": "SlivenCulture", "label": "Culture" },
+        "link2": { "href": "SlivenGeo", "label": "Geography" },
+    },
+    "SlivenCulture": {
+        "title": "LoremC",
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe voluptatum sunt minus voluptatem quaerat quod earum tenetur officia quis.",
+        "link1": { "href": "SlivenLandmarks", "label": "Landmarks" },
+        "link2": { "href": "SlivenGeo", "label": "Geography" },
+    },
+    "SlivenGeo": {
+        "title": "LoremG",
+        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos saepe voluptatum sunt minus voluptatem quaerat quod earum tenetur officia quis.",
+        "link1": { "href": "SlivenLandmarks", "label": "Landmarks" },
+        "link2": { "href": "SlivenCulture", "label": "Culture" },
+    },
+};
--- code-la-vista/JS/popup.js
+++ code-la-vista/JS/popup.js
@@ -1,7 +1,15 @@
-function toggleSlivenGeo() {
-    document.getElementById("SlivenGeo").classList.toggle("active");
+function destroyPopup(item) {
+    item.remove();
 }
 
-function toggleSlivenEco() {
-    document.getElementById("SlivenEco").classList.toggle("active");
+function createPopup(id) {
+    var popup = document.createElement("div");
+    popup.id = id;
+    popup.classList.add('popup');
+    popup.classList.add('active');
+    title = data[id].title;
+    content = data[id].content;
+    popup.innerHTML = `<div class="overlay"></div><div class="content"><div onclick="destroyPopup(${id})" class="close-btn ">&times</div><h2>${title}</h2><div class="text"><p>${content}</p></div><a class="button-1" onclick="destroyPopup(${id}), createPopup('${data[id].link1.href}')">${data[id].link1.label}</a><a class="button-1" onclick="destroyPopup(${id}), createPopup('${data[id].link2.href}')">${data[id].link2.label}</a></div></div>`;
+
+    document.body.appendChild(popup);
 }
